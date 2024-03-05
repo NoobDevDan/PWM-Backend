ac326d1dbf955f1cce92670ca337123dbc72e2dd
JnrDevDan <daniel.lelievre@mibizdata.com.au>
2024-03-05 16:12:17 +1100

    working socket server

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
-const {Server} = require("socket.io");
-const http = require('node:http');
+import  http  from "node:http";
+import { Server } from "socket.io";
 
 const httpServer = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, {
+      "content-type": "text/html"
+    });
+    res.write("connected");
+    res.end();
+  } else {
+    res.writeHead(404).end();
+  }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 httpServer.listen(port, () => {
   console.log(`server listening at http://localhost:${port}`);
 });
 
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: function (origin, callback){
+      if(origin.substring(0,16) === 'http://localhost'){
+        callback(null, true);
+      }
+
+      else{
+        callback(new Error('Not allowed by CORS'), false);
+      }
+    }
+  }
+});
 
 io.on("connection", (socket) => {
   console.log(`connected with transport ${socket.conn.transport.name}`);
 
   socket.conn.on("upgrade", (transport) => {
-    console.log(`transport upgraded to ${transport.name}`);
+    console.log(`connection upgraded to ${transport.name}`);
   });
 
   socket.on("disconnect", (reason) => {
